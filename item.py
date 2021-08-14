56cc03b68ff63c6171e236dd033e600161e8b776
Neel Patel <neelpatel1905.np@gmail.com>
2021-08-14 16:14:18 +0530

    feat: order summery

--- item.py
+++ item.py
@@ -6,6 +6,10 @@ class Item:
         # Get existing json data from file and also assigned 'items' json array to variable for append data, access data and convert to file operations
         self.data = json.load(open('items.json', 'r'))['items']
 
+    def get_all_item(self):
+        # will return all items
+        return self.data
+
     def get_latest_id(self):
         # Adding Length with 1 to get latest id as existing id + 1
         return (len(self.data)+1)
@@ -28,21 +32,38 @@ class Item:
                 except Exception:
                     print("Price must have numerical value")
                     continue 
+
+                try:
+                    available_quantity = int(input("Enter available quantity : "))
+                except Exception:
+                    print("Available Quantity must have numerical value")
+                    continue
                 
                 # If item_type is neither 'addon' nor 'base' then skipping below code 
                 if item_type not in ['addon', 'base']:
                     print("Values must be addon or base")
                     continue
 
-                self.to_file(name, item_type, price)
+                self.to_file(name, item_type, price, available_quantity)
 
             else:
                 break
 
-    def to_file(self, name, item_type, price):
+    def to_file(self, name, item_type, price, available_quantity):
         # generating new ID to append in json file
         id = self.get_latest_id()
         # appended varified and accurate user data
-        self.data.append({'id':id, 'name':name, 'type':item_type, 'price':price})
+        self.data.append({'id':id, 'name':name, 'type':item_type, 'price':price, 'available_quantity':available_quantity})
         # Writing whole file with new json data
         json.dump({'items' : self.data}, open('items.json', 'w'))
+
+    def get_single_item(self, id):
+        # function will return information about single item
+        name = ""
+        price = ""
+        for item in self.data:
+            if id == item['id']:
+                name = item['name']
+                price = item['price']
+                break
+        return {'name':name, 'price':price}
--- order.py
+++ order.py
@@ -1,10 +1,155 @@
+import json
+from item import Item
+
 class Order:
-    __id = 0
     def __init__(self):
-        self.order_dict = {}
-    
+        self.data = json.load(open('orders.json','r'))['orders']
+        self.order_items = []
+
+    def get_latest_id(self):
+        return (len(self.data) + 1)
+
     def make(self):
-        pass
+        while 1:
+            choice = input("Do you want to add item(Y/N) ? : ")
+            if choice.lower() in ['yes','y']:
+                self.show_items()
+                try:
+                    item = int(input("Enter item ID to add in order : "))
+                except:
+                    print("ID should be numeric")
+                    continue
+
+                if not self.validate_id(item):
+                    print("No such item ID exists")
+                    continue
+
+                try:
+                    quantity = int(input("Enter quantity (Max : 3) : "))
+                except:
+                    print("Quantity should be numeric")
+                    continue
+
+                if quantity < 1 or quantity > 3:
+                    print("Quantity should be in range of 1-3 you entered {} \nItem not added".format(quantity))
+                    continue
+
+                self.add_order(item, quantity)
+                
+            else:
+                self.summery()
+                break
+
+    def add_order(self, item, quantity):
+        self.order_items.append({"item":item, 'quantity':quantity})
 
     def summery(self):
-        pass
+        self.show_summery()
+        choice = input("Do you want to add more items(Y/N) : ")
+        if choice.lower() in ['y','yes']:
+            self.make()
+        else:
+            print("Order Placed...") 
+
+    def show_summery(self):
+        for i in range(52):
+            print("-",end="")
+        print("")
+
+        print("| Name                     | Price     | Quantity  |")
+
+        for i in range(52):
+            print("-",end="")
+        print("")
+            
+        total = 0
+        for item in self.order_items:
+            info = Item().get_single_item(item['item'])
+            name = info['name']
+            price = str(info['price'])
+            quantity = str(item['quantity'])
+
+            total = total + (int(price) * int(quantity))
+
+            space = ""
+            for i in range(len(name),25):
+                space = space + " "
+            name = '| ' + name + space
+
+            space = ""
+            for i in range(len(price),10):
+                space = space + " "
+            price = '| ' + price + space
+
+            space = ""
+            for i in range(len(quantity),10):
+                space = space + " "
+            quantity = '| ' + quantity + space + "|"
+
+            print(name + price + quantity)
+
+            for i in range(52):
+                print("-",end="")
+            print("")          
+        
+        total = str(total)
+        space = ""
+        for i in range(len(total), 21):
+            space = space + " "
+        
+        print("| Total                    | {} |".format(total+space))
+        
+        for i in range(52):
+            print("-",end="")
+        print("")   
+
+    def validate_id(self, id):
+        items = Item().get_all_item()
+        found = False
+        for item in items:
+            if item['id'] == id:
+                found = True
+                break
+
+        return found
+
+    def show_items(self):
+        for i in range(0,59):
+            print("-",end="")
+        print("")
+        print("| ID   | Name                     | Price     | Available |")
+        for i in range(0,59):
+            print("-",end="")
+        print("")
+        items = Item().get_all_item()
+        for item in items:
+            id = str(item['id'])
+            name = item['name']
+            price = str(item['price'])
+            quantity = str(item['available_quantity'])
+
+            space = ""
+            for i in range(len(id), 5):
+                space = space + " "
+            id = "| " + id + space
+
+            space = ""
+            for i in range(len(name), 25):
+                space = space + " "
+            name = "| " + name + space
+
+            space = ""
+            for i in range(len(price), 10):
+                space = space + " "
+            price = "| " + price + space
+
+            space = ""
+            for i in range(len(quantity), 10):
+                space = space + " "
+            quantity = "| " + quantity + space + "|"
+
+            print(id + name + price + quantity)
+            
+        for i in range(0,59):
+            print("-",end="")
+        print("")
